7f2f3780673c9daa1da642abe72efbca19248ba6
zy <2211827746@qq.com>
2019-02-16 21:39:53 +0800

    更新XML配置后提交

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: HelloWord
+ * Author:   阿宇
+ * Date:     2019/2/16 0016 21:32
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author 阿宇
+ * @create 2019/2/16 0016
+ * @since 1.0.0
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord!!");
+    }
+}
